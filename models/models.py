5de7a5fa06e7358c2d531f6a640f2de13a7d5e9f
zhimingd2 <shptxc@gmail.com>
2020-01-05 00:07:20 -0500

    update

--- models/models.py
+++ models/models.py
@@ -13,5 +13,22 @@ class CarRequest(models.Model):
     employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True)
     car_id= fields.Many2one(comodel_name="fleet.vehicle", string="Car", required=True)
 
-    state = fields.Selection(string="Status", selection=[('draft','Draft'),('confirm','Confirm'),('validate','Validated'), ('refuse','Refulse'),('approved','Approved')],
-                             default="draft")
+    state = fields.Selection(string="Status", selection=[('draft','Draft'),('confirm','Confirm'),('validate','Validated'),
+                            ('refuse','Refulse'),('approved','Approved')],default="draft")
+
+    @api.multi
+    def confirm_request(self):
+        self.state = 'confirm'
+
+    @api.multi
+    def validate_request(self):
+        self.state = 'validate'
+
+    @api.multi
+    def refuse_request(self):
+        self.state = 'refuse'
+
+    @api.multi
+    def approve_request(self):
+        self.state = 'approved'
+
