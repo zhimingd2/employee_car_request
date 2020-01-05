01a358296396c17f94cf5c3d7ac2ccf4fc4b755b
zhimingd2 <shptxc@gmail.com>
2020-01-04 21:49:39 -0500

    update1

--- __manifest__.py
+++ __manifest__.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "ztest",
+    'name': "Employee Car Request",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Request a car""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Zhiming",
+    'website': "http://www.yourcompany.",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human Resources',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'hr', 'fleet'],
 
     # always loaded
     'data': [
